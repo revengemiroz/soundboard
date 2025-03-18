98daaca0f838c2674e65777a9deae8c98ec713c9
unknown <mirozxy@gmail.com>
2025-03-18 18:25:07 -0400

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,50 +1,36 @@
-import { api } from "@/convex/_generated/api";
-import { fetchQuery } from "convex/nextjs";
-import fs from "fs";
-import { MetadataRoute } from "next";
-import path from "path";
+import type { MetadataRoute } from "next";
 
-const baseUrl = "https://www.instantsoundboard.com";
-const baseDir = "app";
-const excludeDirs = ["api", "fonts"];
-
-export const revalidate = 3600; // Revalidate every hour
-
-async function getRoutes(): Promise<MetadataRoute.Sitemap> {
-  const fullPath = path.join(process.cwd(), baseDir);
-  const entries = fs.readdirSync(fullPath, { withFileTypes: true });
-
-  let routes: string[] = ["/"];
-
-  // Include static routes
-  entries.forEach((entry) => {
-    if (entry.isDirectory() && !excludeDirs.includes(entry.name)) {
-      routes.push(`/${entry.name}`);
-    }
-  });
-
-  // Fetch sound IDs dynamically from Convex API
-  async function getSoundSlugs() {
-    try {
-      const response = await fetchQuery(api.sound.getAllSoundSlugs);
-      // Map IDs to generate dynamic sound routes
-      const soundRoutes: string[] = response.map((sound) => `/sounds/${sound}`);
-      routes = [...routes, ...soundRoutes];
-    } catch (error) {
-      console.error("Error fetching sound slugs:", error);
-    }
-  }
-
-  await getSoundSlugs();
-
-  return routes.map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1.0,
-  }));
-}
-
-export default function sitemap() {
-  return getRoutes();
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://acme.com",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          es: "https://acme.com/es",
+          de: "https://acme.com/de",
+        },
+      },
+    },
+    {
+      url: "https://acme.com/about",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          es: "https://acme.com/es/about",
+          de: "https://acme.com/de/about",
+        },
+      },
+    },
+    {
+      url: "https://acme.com/blog",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          es: "https://acme.com/es/blog",
+          de: "https://acme.com/de/blog",
+        },
+      },
+    },
+  ];
 }
